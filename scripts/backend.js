6d80a5ed556c6536b36ce89a12d66fbab0e5fb8b
kash1ma <ayollo.matvey@gmail.com>
2024-05-27 22:09:18 +0300

    pizdec

---
+++ scripts/backend.js
@@ -0,0 +1,30 @@
+const name = document.getElementById("name").value;
+const phone = document.getElementById("phone").value;
+const date = document.getElementById("date").value;
+const time = document.getElementById("time").value;
+const number = document.getElementById("number").value;
+const comment = document.getElementById("comment").value;
+import fs from "fs-extra";
+// Get the values from the input fields
+
+// Create a JSON object with the reservation data
+const reservation = {
+  name,
+  phone,
+  date,
+  time,
+  number,
+  comment,
+};
+
+// Convert the JSON object to a JSON string
+const jsonData = JSON.stringify(reservation, null, 2);
+
+// Write the JSON data to a file using fs-extra
+fs.writeJSON("data.json", reservation, { spaces: 2 })
+  .then(() => {
+    console.log("Data written successfully");
+  })
+  .catch((error) => {
+    console.error("Error writing data:", error);
+  });
