87ffba269c8cc431b68f15c3552e4396920b22be
kash1ma <ayollo.matvey@gmail.com>
2024-05-27 17:52:56 +0300

    added buttons

---
+++ scripts/reservation.js
@@ -0,0 +1,5 @@
+document.getElementById("popupBtn").addEventListener("click", function () {
+  var popupMenu = document.getElementById("popupMenu");
+  popupMenu.style.display =
+    popupMenu.style.display === "none" ? "block" : "none";
+});
